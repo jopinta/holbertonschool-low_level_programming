275ec46617fcc1576666fa52d4aa65b573dbb347
jopinta <tatinfa@yahoo.com>
2022-01-18 13:36:48 -0800

    bianry up

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -14,12 +14,12 @@ int binary_search(int *array, size_t size, int value)
 {
 size_t lefty = 0;
 size_t righty = size - 1;
-size_t mid;
+size_t mid = 0;
 
 
 if (array == 0)
 return (-1);
-while (lefty < righty)
+ while (lefty <= righty)
 {
 printf("Searching in array: ");
 p_array(array, lefty, righty + 1);
@@ -28,7 +28,7 @@ if (array[mid] < value)
 {
 lefty = mid + 1;
 }
-else if (array[mid] > value)
+ else if (array[mid] > value)
 {
 righty = mid - 1;
 }
@@ -51,13 +51,13 @@ return (-1);
 
 void p_array(int *i, int begg, int end)
 {
-int j;
+        int j;
 
-for (j = begg; j < end; j++)
-{
-if (j < end - 1)
-printf("%d, ", i[j]);
-else
-printf("%d\n ", i[j]);
-}
+        for (j = begg; j < end; j++)
+        {
+	         if (j < end - 1)
+		     printf("%d, ", i[j]);
+		 else
+		     printf("%d\n", i[j]);
+	}
 }
